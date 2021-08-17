69dd71076c3e58e8218e81060d4f1a987c7335b8
lochungtin <lochungtin@gmail.com>
2021-08-17 15:33:27 +0100

    agent utils

--- utils.py
+++ utils.py
@@ -1,13 +1,61 @@
 import numpy as np
 
 
-# softmax function
-def softmax(action_values, tau=1.0):
-    preferences = action_values / tau
-    max_preference = np.max(preferences, axis=1).reshape((-1, 1))
+class Utils:
+    # softmax function
+    def softmax(self, action_values, tau=1.0):
+        preferences = action_values / tau
+        max_preference = np.max(preferences, axis=1).reshape((-1, 1))
 
-    exp_perferences = np.exp(preferences - max_preference)
-    sum_of_exp_preferences = np.sum(exp_perferences, axis=1)
+        exp_perferences = np.exp(preferences - max_preference)
+        sum_of_exp_preferences = np.sum(exp_perferences, axis=1)
 
-    action_probs = exp_perferences / sum_of_exp_preferences.reshape((-1, 1))
-    return action_probs.squeeze()
+        return (exp_perferences / sum_of_exp_preferences.reshape((-1, 1))).squeeze()
+
+    # get temporal difference error
+    def get_td_error(self, states, nState, actions, rewards, discount, terminals, network, curQ, tau):
+        q_next_mat = curQ.get_action_values(nState)
+        probs_mat = self.softmax(q_next_mat, tau)
+        v_next_vec = np.sum(q_next_mat * probs_mat, axis=1) * (1 - terminals)
+        target_vec = rewards + discount * v_next_vec
+
+        q_mat = network.get_action_values(states)
+        batch_indices = np.arange(q_mat.shape[0])
+        q_vec = q_mat[batch_indices, actions]
+
+        return target_vec - q_vec
+
+    # optimize the neural network
+    def optimize_network(self, experiences, discount, optimizer, network, curQ, tau):
+        states, actions, rewards, terminals, nState = map(
+            list,
+            zip(*experiences),
+        )
+        states = np.concatenate(states)
+        nState = np.concatenate(nState)
+        rewards = np.array(rewards)
+        terminals = np.array(terminals)
+        batchSize = states.shape[0]
+
+        delta_vec = self.get_td_error(
+            states,
+            nState,
+            actions,
+            rewards,
+            discount,
+            terminals,
+            network,
+            curQ,
+            tau,
+        )
+
+        batch_indices = np.arange(batchSize)
+
+        delta_mat = np.zeros((batchSize, network.num_actions))
+        delta_mat[batch_indices, actions] = delta_vec
+
+        td_update = network.get_TD_update(states, delta_mat)
+
+        weights = optimizer.update_weights(network.get_weights(), td_update)
+
+        network.set_weights(weights)
